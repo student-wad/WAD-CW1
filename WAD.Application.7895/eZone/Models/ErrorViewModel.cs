863b2e2731041d62eae534fde9d160adc2c5ed3c
student-wad <funit00007895@gmail.com>
2021-02-23 23:16:20 -0800

    Add an empty MVC project

---
+++ WAD.Application.7895/eZone/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eZone.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
