9d35f35675071b760f0ba57025404d82f0224ad5
student-wad <funit00007895@gmail.com>
2021-03-11 16:36:05 -0800

    Add: Properties to Model Teacher

--- WAD.Application.7895/eZone/Models/Teacher.cs
+++ WAD.Application.7895/eZone/Models/Teacher.cs
@@ -7,5 +7,18 @@ namespace eZone.Models
 {
     public class Teacher
     {
+        public int Id { get; set; }
+
+        public int FirstName { get; set; }
+
+        public int LastName { get; set; }
+
+        public DateTime DoB { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public double IELTS_Score { get; set; }
     }
 }
