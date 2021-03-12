251401c0273950bc877092b5c8af1b6b5ec79677
student-wad <funit00007895@gmail.com>
2021-03-12 00:57:54 -0800

    Refactor: Add a Base Repository Class for entities

---
+++ WAD.Application.7895/eZone.DAL/Repositories/BaseRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eZone.DAL.Repositories
+{
+   public abstract class BaseRepo
+    {
+        protected readonly eZoneDbContext _context;
+
+        protected BaseRepo(eZoneDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
