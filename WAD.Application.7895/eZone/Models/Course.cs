57401477128eef200b994a4ed75bafeee4b63606
student-wad <funit00007895@gmail.com>
2021-03-11 16:59:12 -0800

    Add: Validations to Model Course using Data Annotations

--- WAD.Application.7895/eZone/Models/Course.cs
+++ WAD.Application.7895/eZone/Models/Course.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,14 +15,24 @@ namespace eZone.Models
 
     public class Course
     {
+        [Range(0, int.MaxValue)]
         public int Id { get; set; }
 
+        [Required]
+        [DisplayName("Course Level")]
         public CourseLevel CourseLevel { get; set; }
 
+        [Required]
+        [MinLength(5)]
+        [DisplayName("Course Name")]
         public string CourseName { get; set; }
 
+        [Required]
+        [DisplayName("Course Duration")]
         public string CourseDuration { get; set; }
 
+        [Required]
+        [Range(0, float.MaxValue)]
         public float Fee { get; set; }
     }
 }
