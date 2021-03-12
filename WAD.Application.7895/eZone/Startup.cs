ac2fab87550d64dfb59e54818889441f6fb0a6b7
student-wad <funit00007895@gmail.com>
2021-03-11 19:57:42 -0800

    Add: Configuration Services for Database Context

--- WAD.Application.7895/eZone/Startup.cs
+++ WAD.Application.7895/eZone/Startup.cs
@@ -1,6 +1,8 @@
+using eZone.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,9 @@ namespace eZone
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<eZoneDbContext>(
+               options => options.UseSqlServer(
+                   Configuration.GetConnectionString("eZone")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
