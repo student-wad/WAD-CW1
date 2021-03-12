cbc1a39f9149ea2f965a0a87ec02c2046c86cd6f
student-wad <funit00007895@gmail.com>
2021-03-11 16:26:49 -0800

    Add: Properties to Model Group

--- WAD.Application.7895/eZone/Models/Group.cs
+++ WAD.Application.7895/eZone/Models/Group.cs
@@ -5,7 +5,40 @@ using System.Threading.Tasks;
 
 namespace eZone.Models
 {
+    public enum GroupDays
+    {
+        Mon_Wed_Fri,
+        Tue_Thu_Sat
+    }
+
+    public enum GroupLevel
+    {
+        L1,
+        L2,
+        L3
+    }
+
+    public enum GroupTime
+    {
+        _9am,
+        _11am,
+        _3pm,
+        _5pm,
+        _7pm
+    }
+
     public class Group
     {
+        public int Id { get; set; }
+
+        public GroupLevel GroupLevel { get; set; }
+
+        public GroupDays LessonDays { get; set; }
+
+        public GroupTime GroupTime { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public int NumOfStudents { get; set; }
     }
 }
