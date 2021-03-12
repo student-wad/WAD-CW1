0c0d935d109f3e06a025c4de6f5430b1e185d8cc
student-wad <funit00007895@gmail.com>
2021-03-11 16:57:50 -0800

    Add: Properties to Model Course

--- WAD.Application.7895/eZone/Models/Course.cs
+++ WAD.Application.7895/eZone/Models/Course.cs
@@ -5,7 +5,22 @@ using System.Threading.Tasks;
 
 namespace eZone.Models
 {
+    public enum CourseLevel
+    {
+        IELTS,
+        GeneralEnglish
+    }
+
     public class Course
     {
+        public int Id { get; set; }
+
+        public CourseLevel CourseLevel { get; set; }
+
+        public string CourseName { get; set; }
+
+        public string CourseDuration { get; set; }
+
+        public float Fee { get; set; }
     }
 }
