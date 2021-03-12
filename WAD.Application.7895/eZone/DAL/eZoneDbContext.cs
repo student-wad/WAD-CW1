8c733454203ecf4d2689158d97817cceb7fd49d3
student-wad <funit00007895@gmail.com>
2021-03-11 19:08:08 -0800

    Add: Database Context Class

---
+++ WAD.Application.7895/eZone/DAL/eZoneDbContext.cs
@@ -0,0 +1,27 @@
+﻿using eZone.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eZone.DAL
+{
+    public class eZoneDbContext:DbContext
+    {
+        public eZoneDbContext(DbContextOptions<eZoneDbContext> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Course> Courses { get; set; }
+
+        public virtual DbSet<Group> Groups { get; set; }
+
+        public virtual DbSet<Person> People { get; set; }
+
+        public virtual DbSet<Student> Students { get; set; }
+
+        public virtual DbSet<Teacher> Teachers { get; set; }
+    }
+}
