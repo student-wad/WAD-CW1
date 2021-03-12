60c4f918655db7d6ef9883bfe8f638efc7609104
student-wad <funit00007895@gmail.com>
2021-03-11 16:48:11 -0800

    Add: Validations to Model Student using Data Annotations

--- WAD.Application.7895/eZone/Models/Student.cs
+++ WAD.Application.7895/eZone/Models/Student.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,16 +15,29 @@ namespace eZone.Models
 
     public class Student
     {
+        [Range(0, int.MaxValue)]
         public int Id { get; set; }
 
+        [Required]
+        [MinLength(2)]
+        [DisplayName("First Name")]
         public int FirstName { get; set; }
 
+        [Required]
+        [MinLength(2)]
+        [DisplayName("Last Name")]
         public int LastName { get; set; }
 
+        [Required]
+        [Phone]
         public string Phone { get; set; }
 
+        [Required]
+        [DisplayName("First Lesson")]
         public DateTime FirstLesson { get; set; }
 
+        [Required]
+        [DisplayName("Payment Status")]
         public PaymentStatus PaymentStatus { get; set; }
     }
 }
