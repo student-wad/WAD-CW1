95aee3f612f73eccf17848de7dd0b9b749a1c2e1
student-wad <funit00007895@gmail.com>
2021-03-11 23:10:02 -0800

    Fix: Change Descriptions of Enum Values

--- WAD.Application.7895/eZone/Models/Enums.cs
+++ WAD.Application.7895/eZone/Models/Enums.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,10 @@ namespace eZone.Models
 {
     public enum GroupDays
     {
-        Mon_Wed_Fri,
-        Tue_Thu_Sat
+        [Description("Mon/Wed/Fri")]
+        Odd,
+        [Description("Tue/Thu/Sat")]
+        Even
     }
 
     public enum GroupLevel
@@ -20,16 +23,22 @@ namespace eZone.Models
 
     public enum GroupTime
     {
+        [Description("9:00 - 11:00")]
         _9am,
+        [Description("11:00 - 13:00")]
         _11am,
+        [Description("15:00 - 17:00")]
         _3pm,
+        [Description("17:00 - 19:00")]
         _5pm,
+        [Description("19:00 - 21:00")]
         _7pm
     }
 
     public enum PaymentStatus
     {
         Paid,
+        [Description("Not Paid")]
         Not_Paid
     }
 }
