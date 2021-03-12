d09563e9b66cffc03a9f17a939b5fa409191d458
student-wad <funit00007895@gmail.com>
2021-03-12 00:44:17 -0800

    Refactor: Feat: Add methods to IRepository

--- WAD.Application.7895/eZone.DAL/Repositories/IRepository.cs
+++ WAD.Application.7895/eZone.DAL/Repositories/IRepository.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace eZone.DAL.Repositories
 {
-  public interface IRepository
+  public interface IRepository<T> where T:class
     {
+        bool Exists(int id);
+        Task CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+        Task<T> GetByIdAsync(int id);
+        Task<List<T>> GetAllAsync();
     }
 }
