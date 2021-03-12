5e006c7d9cbd741d386849f2c56b2581ed736bbd
student-wad <funit00007895@gmail.com>
2021-03-11 21:56:11 -0800

    Re-do: Add MVC Group Controller with views using EF

---
+++ WAD.Application.7895/eZone/Controllers/GroupController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using eZone.DAL;
+using eZone.Models;
+
+namespace eZone.Controllers
+{
+    public class GroupController : Controller
+    {
+        private readonly eZoneDbContext _context;
+
+        public GroupController(eZoneDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Group
+        public async Task<IActionResult> Index()
+        {
+            var eZoneDbContext = _context.Groups.Include(m => m.Course).Include(m => m.Teacher);
+            return View(await eZoneDbContext.ToListAsync());
+        }
+
+        // GET: Group/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var group = await _context.Groups
+                .Include(m => m.Course)
+                .Include(m => m.Teacher)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (group == null)
+            {
+                return NotFound();
+            }
+
+            return View(group);
+        }
+
+        // GET: Group/Create
+        public IActionResult Create()
+        {
+            ViewData["CourseId"] = new SelectList(_context.Courses, "Id", "CourseDuration");
+            ViewData["TeacherId"] = new SelectList(_context.Teachers, "Id", "Email");
+            return View();
+        }
+
+        // POST: Group/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,GroupLevel,LessonDays,GroupTime,StartDate,NumOfStudents,CourseId,TeacherId")] Group group)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(group);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CourseId"] = new SelectList(_context.Courses, "Id", "CourseDuration", group.CourseId);
+            ViewData["TeacherId"] = new SelectList(_context.Teachers, "Id", "Email", group.TeacherId);
+            return View(group);
+        }
+
+        // GET: Group/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var group = await _context.Groups.FindAsync(id);
+            if (group == null)
+            {
+                return NotFound();
+            }
+            ViewData["CourseId"] = new SelectList(_context.Courses, "Id", "CourseDuration", group.CourseId);
+            ViewData["TeacherId"] = new SelectList(_context.Teachers, "Id", "Email", group.TeacherId);
+            return View(group);
+        }
+
+        // POST: Group/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,GroupLevel,LessonDays,GroupTime,StartDate,NumOfStudents,CourseId,TeacherId")] Group group)
+        {
+            if (id != group.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(group);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GroupExists(group.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CourseId"] = new SelectList(_context.Courses, "Id", "CourseDuration", group.CourseId);
+            ViewData["TeacherId"] = new SelectList(_context.Teachers, "Id", "Email", group.TeacherId);
+            return View(group);
+        }
+
+        // GET: Group/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var group = await _context.Groups
+                .Include(m => m.Course)
+                .Include(m => m.Teacher)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (group == null)
+            {
+                return NotFound();
+            }
+
+            return View(group);
+        }
+
+        // POST: Group/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var group = await _context.Groups.FindAsync(id);
+            _context.Groups.Remove(group);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GroupExists(int id)
+        {
+            return _context.Groups.Any(e => e.Id == id);
+        }
+    }
+}
