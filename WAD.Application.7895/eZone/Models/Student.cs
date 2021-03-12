c476d82c9d60a8d457b35f5c6e5a863751576d59
student-wad <funit00007895@gmail.com>
2021-03-11 16:42:25 -0800

    Add: Properties to Model Student

--- WAD.Application.7895/eZone/Models/Student.cs
+++ WAD.Application.7895/eZone/Models/Student.cs
@@ -5,7 +5,24 @@ using System.Threading.Tasks;
 
 namespace eZone.Models
 {
+    public enum PaymentStatus
+    {
+        Paid,
+        Not_Paid
+    }
+
     public class Student
     {
+        public int Id { get; set; }
+
+        public int FirstName { get; set; }
+
+        public int LastName { get; set; }
+
+        public string Phone { get; set; }
+
+        public DateTime FirstLesson { get; set; }
+
+        public PaymentStatus PaymentStatus { get; set; }
     }
 }
