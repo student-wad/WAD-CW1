eb6549e6d365413e5f6d491ae1f2d54e0cbf7722
student-wad <funit00007895@gmail.com>
2021-03-20 17:23:44 -0700

    Refactor: Add Migration Inheritance

---
+++ WAD.Application.7895/eZone.DAL/Migrations/20210321001955_Inheritance.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eZone.DAL.Migrations
+{
+    public partial class Inheritance : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Groups_Teachers_TeacherId",
+                table: "Groups");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Teachers",
+                table: "Teachers");
+
+            migrationBuilder.RenameTable(
+                name: "Teachers",
+                newName: "People");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "IELTS_Score",
+                table: "People",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "People",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DoB",
+                table: "People",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "People",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FirstLesson",
+                table: "People",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "GroupId",
+                table: "People",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PaymentStatus",
+                table: "People",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_People",
+                table: "People",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_People_GroupId",
+                table: "People",
+                column: "GroupId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Groups_People_TeacherId",
+                table: "Groups",
+                column: "TeacherId",
+                principalTable: "People",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_People_Groups_GroupId",
+                table: "People",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Groups_People_TeacherId",
+                table: "Groups");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_People_Groups_GroupId",
+                table: "People");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_People",
+                table: "People");
+
+            migrationBuilder.DropIndex(
+                name: "IX_People_GroupId",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "FirstLesson",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "GroupId",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentStatus",
+                table: "People");
+
+            migrationBuilder.RenameTable(
+                name: "People",
+                newName: "Teachers");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "IELTS_Score",
+                table: "Teachers",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Teachers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DoB",
+                table: "Teachers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Teachers",
+                table: "Teachers",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstLesson = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GroupId = table.Column<int>(type: "int", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PaymentStatus = table.Column<int>(type: "int", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Students_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_GroupId",
+                table: "Students",
+                column: "GroupId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Groups_Teachers_TeacherId",
+                table: "Groups",
+                column: "TeacherId",
+                principalTable: "Teachers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
