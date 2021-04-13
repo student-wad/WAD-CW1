1d67cf2b156ea645c7f081da86a5400ab8e6fa1a
student-wad <funit00007895@gmail.com>
2021-04-13 13:58:17 -0700

    Feat: Add Enum Extensions

---
+++ WAD.Application.7895/eZone.BLL/EnumExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eZone.BLL
+{
+   public static class EnumExtensions
+    {
+        public static TAttribute GetAttribute<TAttribute>(this Enum enumValue)
+               where TAttribute : Attribute
+        {
+            return enumValue.GetType()
+                            .GetMember(enumValue.ToString())
+                            .First()
+                            .GetCustomAttribute<TAttribute>();
+        }
+
+       
+    }
+}
