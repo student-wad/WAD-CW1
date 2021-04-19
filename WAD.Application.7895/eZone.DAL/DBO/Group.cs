d740cc0bf815f5b236e7ca3b4a14fdff9e3d4e65
student-wad <funit00007895@gmail.com>
2021-04-19 02:29:15 -0700

    Revert "Feat: SPA: Add addEditTeacher page for Teacher entity"
    This reverts commit 407d8552c2f50284b9087f59f728fb65a9e247c5.

--- WAD.Application.7895/eZone.DAL/DBO/Group.cs
+++ WAD.Application.7895/eZone.DAL/DBO/Group.cs
@@ -27,7 +27,7 @@ namespace eZone.DAL.DBO
 
         [Required]
         [DisplayName("Start Date")]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]        
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime StartDate { get; set; }
 
         [Required]
