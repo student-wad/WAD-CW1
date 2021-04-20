573ae7860e75f22bb0b2e7da3c373a76ff8ce28c
student-wad <funit00007895@gmail.com>
2021-04-20 16:39:49 -0700

    Fix: Remove internal dbSet; implement _context.Set<T> in CRUD

--- WAD.Application.7895/eZone.DAL/Repositories/BaseRepo.cs
+++ WAD.Application.7895/eZone.DAL/Repositories/BaseRepo.cs
@@ -6,27 +6,25 @@ namespace eZone.DAL.Repositories
 {
     public abstract class BaseRepo<T>:IRepository<T> where T:class
     {
-        protected readonly eZoneDbContext _context;
-        internal DbSet<T> _dbSet;
+        protected readonly eZoneDbContext _context;        
         protected BaseRepo(eZoneDbContext context)
         {
-            _context = context;
-            _dbSet = _context.Set<T>();
+            _context = context;           
         }
         public async Task CreateAsync(T entity)
         {
-            _context.Add(entity);
+            _context.Set<T>().Add(entity);
             await _context.SaveChangesAsync();
         }
         public async Task UpdateAsync(T entity)
         {
-            _context.Update(entity);
+            _context.Set<T>().Update(entity);
             await _context.SaveChangesAsync();
         }
         public async Task DeleteAsync(int id)
         {
-            var entity = await _dbSet.FindAsync(id);
-            _dbSet.Remove(entity);
+            var entity = await _context.Set<T>().FindAsync(id);
+            _context.Set<T>().Remove(entity);
             await _context.SaveChangesAsync();
         }
         public abstract bool Exists(int id);
