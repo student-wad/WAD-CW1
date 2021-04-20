4802a0263a61e779c34b41dc01f300d8afde6174
student-wad <funit00007895@gmail.com>
2021-04-20 02:26:47 -0700

    Feat: Add an EnumValue class

---
+++ WAD.Application.7895/eZone.BLL/Enums/EnumValue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eZone.BLL.Enums
+{
+   public class EnumValue
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+    }
+}
